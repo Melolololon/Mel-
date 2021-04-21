0739a9096cf62f5f6337573a7d7d72c5c5d60398
Melolololon <75592470+Melolololon@users.noreply.github.com>
2021-04-21 14:25:36 +0900

    クラス化開始

--- MyLibrary/Model3D.h
+++ MyLibrary/Model3D.h
@@ -25,8 +25,34 @@ private:
 	std::vector < ModelConstData>modelConstData;
 	MaterialConstBuffData material;
 
-	std::vector <Model3D* > parentModel;
+	std::vector <Model3D*> parentModel;
 	std::vector <int> parentModelNumbers;
+
+	//定数バッファのタグ(ルートパラメーターセット用)
+	//"Texture" シェーダーリソースビュー t0
+	//"LibraryConst" ライブラリ構造体 b0
+	//"UserConst" ユーザー構造体 b1
+	//"Material" マテリアル構造体 b2
+	//COMMON_CONST_BUFFER 共通 b3
+	enum ConstBufferTag
+	{
+		TEXTURE_BUFFER,
+		COMMON_CONST_BUFFER,
+		LIBRARY_CONST_BUFFER,
+		USER_CONST_BUFFER,
+		MATERIAL_CONST_BUFFER,
+		OBJ_BONE_MATRIX_CONST_BUFFER
+	};
+	std::vector<ConstBufferTag>heapTags;
+
+	//[objの中のモデルごと]
+	std::vector<VertexBufferSet> vertexBufferSet;
+	std::vector<IndexBufferSet>indexBufferSet;
+
+	std::vector<ConstBufferSet> constBufferSet;
+	ComPtr<ID3D12Resource>commonBuffers;
+	TextureBufferSet textureBufferSet;
+
 public:
 	Model3D();
 	~Model3D();
