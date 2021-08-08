4ea49ec4ec516523828cacf646492293de05c669
Melolololon <75592470+Melolololon@users.noreply.github.com>
2021-08-09 03:48:34 +0900

    これからテクスチャを改良

--- MyLibrary/Texture3D.cpp
+++ MyLibrary/Texture3D.cpp
@@ -1,1 +1,6 @@
 #include "Texture3D.h"
+
+bool MelLib::Texture3D::Load(const std::vector<std::string>& path, const std::string& name)
+{
+    return false;
+}
--- MyLibrary/TextureLoader.cpp
+++ MyLibrary/TextureLoader.cpp
@@ -2,42 +2,53 @@
 
 
 using namespace MelLib;
+//
+//BMPData TextureLoader::LoadBMP(const char* texturepath)
+//{
+//	FILE* texture;
+//	fopen_s(&texture, texturepath, "rb");
+//	//パスでファイル形式を確認する
+//	int size = strlen(texturepath);
+//
+//	if (texturepath[size - 1] != 'p')
+//	{
+//		//戻り値をvoidにして、構造体は引数で受け取るようにする
+//		BMPData null;
+//		return null;
+//	}
+//
+//	BMPData bmpData;
+//	fread(&bmpData, sizeof(bmpData) - 16, 1, texture);
+//
+//	if (bmpData.bit == 32)
+//	{
+//		unsigned char sig[84];
+//		fread(&sig, sizeof(sig), 1, texture);
+//	}
+//
+//	bmpData.rgbaData.resize(bmpData.height * bmpData.width);
+//	fread(bmpData.rgbaData.data(), bmpData.rgbaData.size() * 4, 1, texture);
+//
+//	std::reverse(bmpData.rgbaData.begin(), bmpData.rgbaData.end());
+//	for (int i = 0; i < (int)bmpData.height; i++)
+//	{
+//		std::reverse(bmpData.rgbaData.begin() + bmpData.width * i, bmpData.rgbaData.begin() + bmpData.width * i + bmpData.width);
+//	}
+//
+//	fclose(texture);
+//
+//
+//	return bmpData;
+//	
+//}
 
-BMPData TextureLoader::LoadBMP(const char* texturepath)
+TextureLoader* MelLib::TextureLoader::GetInstance()
 {
-	FILE* texture;
-	fopen_s(&texture, texturepath, "rb");
-	//パスでファイル形式を確認する
-	int size = strlen(texturepath);
+	static TextureLoader t;
+	return &t;
+}
 
-	if (texturepath[size - 1] != 'p')
-	{
-		//戻り値をvoidにして、構造体は引数で受け取るようにする
-		BMPData null;
-		return null;
-	}
-
-	BMPData bmpData;
-	fread(&bmpData, sizeof(bmpData) - 16, 1, texture);
-
-	if (bmpData.bit == 32)
-	{
-		unsigned char sig[84];
-		fread(&sig, sizeof(sig), 1, texture);
-	}
-
-	bmpData.rgbaData.resize(bmpData.height * bmpData.width);
-	fread(bmpData.rgbaData.data(), bmpData.rgbaData.size() * 4, 1, texture);
-
-	std::reverse(bmpData.rgbaData.begin(), bmpData.rgbaData.end());
-	for (int i = 0; i < (int)bmpData.height; i++)
-	{
-		std::reverse(bmpData.rgbaData.begin() + bmpData.width * i, bmpData.rgbaData.begin() + bmpData.width * i + bmpData.width);
-	}
-
-	fclose(texture);
-
-
-	return bmpData;
-	
+bool MelLib::TextureLoader::LoadTexture(const std::vector<std::string>& path, std::vector<DirectX::TexMetadata>& metadata, std::vector<DirectX::ScratchImage>& scratchImage)
+{
+	return false;
 }
